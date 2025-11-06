d5cd43b605c7bfb4e9e543eb04d7a1fb58983f33
Dmitry <gmoxa@rambler.ru>
2025-11-06 22:43:22 +0300

    change cl.yml 2

--- aa/tests.py
+++ aa/tests.py
@@ -1,12 +1,5 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
 
 class MyTest(TestCase):
     def test_ok(self):
         self.assertTrue(True, True)
-
-    def test_fail(self):
-        url = "/admin/"
-        client = APIClient()
-        response = client.get(url)
-        self.assertEqual(response.status_code, 302)
