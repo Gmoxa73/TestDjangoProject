62e09fc038072967536ccca021843d3c5c24c700
Dmitry <gmoxa@rambler.ru>
2025-11-06 22:25:41 +0300

    change cl.yml 2

--- aa/tests.py
+++ aa/tests.py
@@ -4,9 +4,3 @@ from rest_framework.test import APIClient
 class MyTest(TestCase):
     def test_ok(self):
         self.assertTrue(True, True)
-
-    def test_fail(self):
-        url = "/"
-        client = APIClient()
-        response = client.get(url)
-        self.assertEqual(response.status_code, 200)
