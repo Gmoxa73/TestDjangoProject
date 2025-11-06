84d3482e604b0df46b6fe700addf147bec16ecef
Dmitry <gmoxa@rambler.ru>
2025-11-06 23:12:29 +0300

    change cl.yml 2

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DJANGO_DB"),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DJANGO_DB_NAME"),
         'USER': os.getenv("DJANGO_DB_USER"),
         'PASSWORD': os.getenv("DJANGO_DB_PASSWORD"),
-        'HOST': os.getenv("DJANGO_DB_HOST"),
-        'PORT': os.getenv("DJANGO_DB_PORT"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
