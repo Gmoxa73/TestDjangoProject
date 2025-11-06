7af170cf7194ea6751934ce7bad44c82c6f80ade
Dmitry <gmoxa@rambler.ru>
2025-11-06 17:57:30 +0300

    add cl.yml

--- aa/tests.py
+++ aa/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class MyTest(TestCase):
+    def test_ok(self):
+        self.assertTrue(True, True)
+
+    def test_fail(self):
+        url = "/"
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
